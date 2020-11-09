90059370a9e75715301711ab70a8c0b8fce7510b
Oli Weighell <oli.weighell@digital.justice.gov.uk>
2020-11-09 09:44:04 +0000

    Update CIS and CCMS relationship to single 'uses' line

--- src/main/kotlin/model/CCMS.kt
+++ src/main/kotlin/model/CCMS.kt
@@ -56,8 +56,10 @@ class CCMS private constructor() {
 
     override fun defineRelationships() {
       soa.uses(BenefitChecker.system, "validates Universal Credit claimants via", "SOAP")
-      soa.uses(CIS.system, "imports invoices approved for payment from")
-      soa.uses(CIS.system, "updates status of paid invoices in")
+      soa.uses(
+        CIS.system,
+        "imports CIS invoices approved for payment and, after payment, updates status of invoices in CIS"
+      )
     }
 
     override fun defineExternalRelationships() {
