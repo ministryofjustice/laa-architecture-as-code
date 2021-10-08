9f05332036f19a7fed8f4a449c56e950de280cdd
willc-work <william.clarke@digital.justice.gov.uk>
2021-10-08 12:04:00 +0100

    add Legal Framework API to apply architecture diagram

---
+++ src/main/kotlin/model/LFA.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.laa.architecture
+
+import com.structurizr.model.Container
+import com.structurizr.model.Model
+import com.structurizr.model.SoftwareSystem
+import com.structurizr.view.AutomaticLayout
+import com.structurizr.view.ViewSet
+
+class LFA private constructor() {
+  companion object : LAASoftwareSystem {
+    lateinit var system: SoftwareSystem
+    lateinit var api: Container
+
+    override fun defineModelEntities(model: Model) {
+      system = model.addSoftwareSystem(
+        "Legal Framework API",
+        "A service for checking means and merits requirements for Legal Aid Applications"
+      ).apply {
+        Tags.GET_LEGAL_AID.addTo(this)
+      }
+
+      api = system.addContainer(
+        "Legal Framework API",
+        "An API for Legal Framework",
+        "Ruby on Rails"
+      ).apply {
+        setUrl("https://github.com/ministryofjustice/legal-framework-api")
+      }
+    }
+
+    override fun defineInternalContainerRelationships() {
+    }
+
+    override fun defineRelationships() {
+    }
+
+    override fun defineExternalRelationships() {
+    }
+
+    override fun defineUserRelationships() {
+    }
+
+    override fun defineViews(views: ViewSet) {
+      views.createSystemContextView(system, "lfa-context", null).apply {
+        addDefaultElements()
+        enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300)
+      }
+
+      views.createContainerView(system, "lfa-container", null).apply {
+        addDefaultElements()
+        enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300)
+      }
+    }
+  }
+}
